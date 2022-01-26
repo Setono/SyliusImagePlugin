0efcc13a784fec15ffcf96b5f662b2d13cc8597e
Joachim Løvgaard <joachim@loevgaard.dk>
2022-01-26 12:40:34 +0100

    Initial rename

--- src/SetonoSyliusImagePlugin.php
+++ src/SetonoSyliusImagePlugin.php
@@ -2,12 +2,12 @@
 
 declare(strict_types=1);
 
-namespace Acme\SyliusExamplePlugin;
+namespace Setono\SyliusImagePlugin;
 
 use Sylius\Bundle\CoreBundle\Application\SyliusPluginTrait;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
-final class AcmeSyliusExamplePlugin extends Bundle
+final class SetonoSyliusImagePlugin extends Bundle
 {
     use SyliusPluginTrait;
 }
