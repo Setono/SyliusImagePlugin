1df7e74ff5259b7748ec19f977ea77f3d67104bd
Flemming Madsen <mail@leflings.dk>
2022-03-11 09:23:01 +0100

    Also reset retryAt when processing finishes

--- src/EventSubscriber/ResetProcessingRetryStatusSubscriber.php
+++ src/EventSubscriber/ResetProcessingRetryStatusSubscriber.php
@@ -10,23 +10,24 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\Workflow\Event\Event;
 use Webmozart\Assert\Assert;
 
-final class ResetTriesSubscriber implements EventSubscriberInterface
+final class ResetProcessingRetryStatusSubscriber implements EventSubscriberInterface
 {
     public static function getSubscribedEvents(): array
     {
         $event = sprintf('workflow.%s.completed.%s', ProcessWorkflow::NAME, ProcessWorkflow::TRANSITION_FINISH);
 
         return [
-            $event => 'resetTries',
+            $event => 'resetProcessingRetryStatus',
         ];
     }
 
-    public function resetTries(Event $event): void
+    public function resetProcessingRetryStatus(Event $event): void
     {
         /** @var ImageInterface|object $image */
         $image = $event->getSubject();
         Assert::isInstanceOf($image, ImageInterface::class);
 
         $image->resetProcessingTries();
+        $image->setProcessingRetryAt(null);
     }
 }
