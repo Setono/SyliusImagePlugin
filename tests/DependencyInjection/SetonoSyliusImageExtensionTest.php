90c6bed7e677d052b0bee6b1d979d6717574bd8d
Flemming Madsen <mail@leflings.dk>
2022-03-15 10:17:25 +0100

    Fix unit test

--- tests/DependencyInjection/SetonoSyliusImageExtensionTest.php
+++ tests/DependencyInjection/SetonoSyliusImageExtensionTest.php
@@ -24,7 +24,9 @@ final class SetonoSyliusImageExtensionTest extends AbstractExtensionTestCase
      */
     public function after_loading_the_correct_parameter_has_been_set(): void
     {
+        $this->setParameter('setono_sylius_image.public_processed_path', '/media/image/processed');
         $this->load();
+
 
         $this->assertContainerBuilderHasParameter('setono_sylius_image.public_processed_path', '/media/image/processed');
         $this->assertContainerBuilderHasParameter('setono_sylius_image.available_variants', []);
